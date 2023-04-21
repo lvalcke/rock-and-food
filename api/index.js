7ebef489bae13e530290d4c9133027fa466bb3dd
lvalcke <lvalcke88@gmail.com>
2023-04-21 02:30:56 -0500

    config/mongoose DB

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 require('dotenv').config();
+const { USER, PASSWORD } = process.env;
 const server = require('./src/app.js');
+const mongoose = require('mongoose');
 
 const PORT = process.env.PORT || 3001;
 
-server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@cluster-levo.1zvw9yg.mongodb.net/rock-and-food`)
+  .then(() => {
+    console.log('Connected to mongoDB...');
+    server.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}...`);
+    });
+  });
