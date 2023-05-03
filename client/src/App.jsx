50b8c533e157281983e39613cb044e5a8fbf5ec6
lvalcke <lvalcke88@gmail.com>
2023-05-03 02:59:28 -0500

    create/vite react app - redux

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  
+  return (
+    <>
+      <Routes>
+        <Route />
+      </Routes>
+
+    </>
+  );
+}
+
+export default App;
